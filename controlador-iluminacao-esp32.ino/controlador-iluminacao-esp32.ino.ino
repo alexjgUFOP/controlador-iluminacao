a3309a5af1be7cc0d8dd628d570f0eb7a7c99942
alexjgUFOP <b9a3b00071f752154b6da1926d300c6ef5628c88>
2023-01-20 03:10:05 -0300

    Adição base dos arquivos

---
+++ controlador-iluminacao-esp32.ino/controlador-iluminacao-esp32.ino.ino
@@ -0,0 +1,20 @@
+     
+/*
+  Controlador de Iluminacao com ESP32
+  
+  Compilador: Arduino 1.8.19
+
+    
+*/
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
